6628dbf6d8b0b9055dc4f46dd43f45bd99762dd5
sumaila <sumailafarheen1998>
2021-02-01 20:26:18 +0530

    displayed show results and small refactor

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -6,7 +6,7 @@ import Title from './Title';
 const MainPageLayout = ({ children }) => {
     return (
         <div>
-            <Title title="Box Office" subtitle="Are you looking for a movir or an actor?" />
+            <Title title="Box Office" subtitle="Are you looking for a movie or an actor?" />
             <Navs />
             {children}
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,54 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-template-curly-in-string */
 /* eslint-disable arrow-body-style */
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout';
+import {apiGet} from '../misc/config'
 
 
 const Home = () => {
-    return <MainPageLayout>This is Home</MainPageLayout>;
-    
+     const [ input,setInput ] = useState('');
+     const [results, setResults] = useState(null);
+
+     const onSearch = () => {
+      apiGet('/search/shows?q=${input}').then(result => {
+        setResults(result);
+        });
+    };
+     
+    const onInputChange = ev => {
+        setInput(ev.target.value);
+    };
+
+    const onKeyDown = ev => {
+        if (ev.keyCode === 13) {
+            onSearch();
+        }
+    };
+
+    const renderResults = () => {
+        if(results && results.length === 0){
+            return <div>No Results</div>;
+        }
+        if(results && results.length > 0) {
+            return (
+                 <div>
+               {results.map(item => (
+                <div key={item.show.id}>{item.show.name}</div>   
+               ))}
+            </div>
+            );
+    }
+      return null;
 };
+     return (
+        <MainPageLayout>
+        <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+        <button type="button" onClick={onSearch}>Search</button>
+        </MainPageLayout>
+    );  
+     };
 
 export default Home;
